0608e792def55789982072fa3fb54b660a98039d
Himani <14.himani@gmail.com>
2020-09-08 10:24:08 -0400

    Create ArrayofFiveNum.java

---
+++ Challenge/src/ArrayofFiveNum.java
@@ -0,0 +1,13 @@
+public class ArrayofFiveNum {
+    public static void main(String[] args) {
+        int[] num = new int[5];
+        num[0] = 0;
+        num[1] = 3;
+        num[2] = 6;
+        num[3] = 9;
+        num[4] = 12;
+        for (int i = 0; i<num.length;i++) {
+            System.out.println(num[i]);
+        }
+    }
+}
